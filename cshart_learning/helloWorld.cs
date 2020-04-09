1c7072ef4a0d43cddc5abbcc48a7cedd93448b20
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-04-08 20:56:30 -0400

    Create helloWorld.cs

---
+++ cshart_learning/helloWorld.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorld
+{
+    //A simple Hello World C# program
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello World");
+            Console.Read();
+        }
+    }
+
+}
