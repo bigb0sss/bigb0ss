af0a7c9ea36c204db4bdd2f88794a5c5547d506c
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-10-16 21:50:33 -0400

    Create basic_1-5.py

---
+++ python_learning/w3resource/basics/basic_1-5.py
@@ -0,0 +1,8 @@
+# 5. Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+
+firstName = input("[*] First Name: ")
+firstName = str(firstName)
+lastName = input("[*] Last Name: ")
+lastName = str(lastName)
+
+print("[+] Name:", lastName, firstName)
