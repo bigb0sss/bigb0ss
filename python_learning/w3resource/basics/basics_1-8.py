ce43add327f2b674198a6ab62b72abe46e168019
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-10-18 10:45:22 -0400

    Create basics_1-8.py

---
+++ python_learning/w3resource/basics/basics_1-8.py
@@ -0,0 +1,7 @@
+# 8. Write a Python program to display the first and last colors from the following list.
+# color_list = ["Red","Green","White" ,"Black"]
+
+color_list = ["Red","Green","White" ,"Black"]
+
+print("[+] First Item: {0}".format(color_list[0]))
+print("[+] Last Item : {0}".format(color_list[-1]))
